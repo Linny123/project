0ce319a86c86765d937a6089d0075c8225f592b4
linda de corte <lindadecorte@dhcp-144-56.vub.ac.be>
2014-11-21 10:57:40 +0100

    Initial Commit

---
+++ taak3-ldecorte/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  taak3-ldecorte
+//
+//  Created by linda de corte on 21-11-14.
+//  Copyright (c) 2014 linda de corte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
